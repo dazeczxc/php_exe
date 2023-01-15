618e386050f4b3dceedbabaaef24650189a0f3b6
dazec <blackdazec@gmail.com>
2023-01-15 19:31:48 +0800

    add bg on result

--- exercise/exercise2.php
+++ exercise/exercise2.php
@@ -26,44 +26,44 @@
   <p>Result:</p>
 
   <div class="codeBg">
-  <?php
+    <?php
 
-    echo "<pre>";
-    // Starting the outer loop for rows
-    for ($row = 1; $row <= 11; $row++) {
-        // Starting the inner loop for columns
-        for ($col = 1; $col <= 11; $col++) {
-            // Checking if the current row and column match the conditions 
-            if ($row == $col || $row + $col == 12) {
-                // Checking if the current row and column is 6,6
-                if($row == 6 && $col == 6){
-                    echo "1";
-                }
-                // Checking if the current row is 2 or 10
-                elseif($row == 2 || $row == 10){
-                    echo "5";
-                } 
-                // Checking if the current row is 4 or 8
-                elseif($row == 4 || $row == 8){
-                    echo "3";
-                } 
-                // If none of the above conditions are met
-                else {
-                    echo "*";
-                }
-            } 
-            // If the current row and column do not match the conditions
-            else {
-                echo "  ";
-            }
-        }
-        // Creating a new line after each row
-        echo "\n";
-    }
-    // Closing the table
-    echo " </pre>";
-    
-  ?>
+      echo "<pre>";
+      // Starting the outer loop for rows
+      for ($row = 1; $row <= 11; $row++) {
+          // Starting the inner loop for columns
+          for ($col = 1; $col <= 11; $col++) {
+              // Checking if the current row and column match the conditions 
+              if ($row == $col || $row + $col == 12) {
+                  // Checking if the current row and column is 6,6
+                  if($row == 6 && $col == 6){
+                      echo "1";
+                  }
+                  // Checking if the current row is 2 or 10
+                  elseif($row == 2 || $row == 10){
+                      echo "5";
+                  } 
+                  // Checking if the current row is 4 or 8
+                  elseif($row == 4 || $row == 8){
+                      echo "3";
+                  } 
+                  // If none of the above conditions are met
+                  else {
+                      echo "*";
+                  }
+              } 
+              // If the current row and column do not match the conditions
+              else {
+                  echo "  ";
+              }
+          }
+          // Creating a new line after each row
+          echo "\n";
+      }
+      // Closing the table
+      echo " </pre>";
+      
+    ?>
   </div>
 
 </div>
